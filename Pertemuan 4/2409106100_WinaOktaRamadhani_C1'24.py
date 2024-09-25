d2ea0c7fcc567962cf7fb70c23cec3ffac308fb6
winaokta1106 <inaaramadhani0@gmail.com>
2024-09-25 16:17:41 +0800

    Add files via upload

---
+++ Pertemuan 4/2409106100_WinaOktaRamadhani_C1'24.py
@@ -0,0 +1,57 @@
+# ulang = 0
+# for i in range (ulang):
+#     print("halo")\
+
+# simpan = [12, "udin petot", 14.5, True, 'A', "102"]
+# for i in simpan[2:5]:
+#     print(i)
+# print(simpan)
+# print(simpan[4])
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("_________________________________")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Bakso", "Soto"]
+# for i in simpan:
+#     print(i)
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("_________________________________")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Bakso", "Soto"]
+# for i, menu in enumerate(simpan,start=100):
+#     print(f"{i}.{menu}")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("_________________________________")
+# simpan = ["Nasi Goreng", "Mie Goreng", "Bakso", "Soto"]
+# for i in range(len(simpan)):
+#     print(f"{i+1}. {simpan[i]}")
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya')
+# hitung += 1
+# jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+# jawab = 'ya'
+# hitung = 'ya'
+# while(True):
+#     hitung += 1
+#     jawaba = input("Balikan lagi tidak?")
+#     if jawab == "ga" or jawab == "iya" :
+#         break
+
+# print("Daftar Bilangan Ganjil dari 1-10")
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin lanjut? ")
+#     if ulang == "y" or ulang =="Y":
+#         print("Oke Kita Lanjut")
+#         continue
+
